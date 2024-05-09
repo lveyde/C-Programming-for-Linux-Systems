37b35b0863c9ea4d309022d5890097efe9349a99
Lev Veyde <lveyde@gmail.com>
2024-05-09 17:05:30 +0300

    Fixed thread_creation_example.cpp
    - Updated formatting.
    - Fixed the issue of mixed output from different threads.
    
    Signed-off-by: Lev Veyde <lveyde@gmail.com>

--- Chapter 2/thread_creation_example.cpp
+++ Chapter 2/thread_creation_example.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
 #include <chrono>
+#include <iostream>
+#include <sstream>
 #include <thread>
 using namespace std;
 using namespace std::chrono;
+
 void detached_routine() {
     cout << "Starting detached_routine thread.\n";
     this_thread::sleep_for(seconds(2));
@@ -16,23 +18,30 @@ void joined_routine() {
 }
 
 void thread_creator() {
+    stringstream msg;
     cout << "Starting thread_creator.\n";
     thread t1(detached_routine);
-    cout << "Before - Is the detached thread joinable: "
-         << t1.joinable() << endl;
+    msg << "Before - Is the detached thread joinable: " << t1.joinable()
+        << endl;
+    cout << msg.str();
     t1.detach();
-    cout << "After - Is the detached thread joinable: "
-         << t1.joinable() << endl;
+    msg.str("");
+    msg.clear();
+    msg << "After - Is the detached thread joinable: " << t1.joinable() << endl;
+    cout << msg.str();
+
     thread t2(joined_routine);
-    cout << "Before - Is the joined thread joinable: "
-         << t2.joinable() << endl;
+    msg.str("");
+    msg.clear();
+    msg << "Before - Is the joined thread joinable: " << t2.joinable() << endl;
+    cout << msg.str();
     t2.join();
-    cout << "After - Is the joined thread joinable: "
-         << t2.joinable() << endl;
+    msg.str("");
+    msg.clear();
+    msg << "After - Is the joined thread joinable: " << t2.joinable() << endl;
+    cout << msg.str();
     this_thread::sleep_for(chrono::seconds(1));
     cout << "Exiting thread_creator.\n";
 }
- 
-int main() {
-    thread_creator();
-}
+
+int main() { thread_creator(); }
